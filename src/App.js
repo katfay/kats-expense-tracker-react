fa9b6d409253ec359b6f3e158d72cef155895325
Kat Fay <101537105+katfay@users.noreply.github.com>
2023-02-13 06:51:10 +1100

    Move components into App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from "react";
 
+import Banner from "./Banner.js";
+import AddButton from "./AddButton.js";
+import Dashboard from "./Dashboard.js";
+import AddExpensePopUp from "./AddExpensePopUp.js";
+
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Test</div>;
+  return (
+    <div className="App">
+      Test
+      <Banner />
+      <AddButton />
+      <Dashboard />
+      <AddExpensePopUp />
+    </div>
+  );
 }
