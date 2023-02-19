2fe261ebb61dc7d7534ddbdfe47ab7ad183d7cf1
Kat Fay <101537105+katfay@users.noreply.github.com>
2023-02-19 13:06:02 +1100

    Replace table with form

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -5,31 +5,18 @@ import "./Dashboard.css";
 export default function Dashboard() {
   return (
     <div className="Dashboard">
-      <table>
-        <thead>
-          <td>Expense</td>
-          <td>Amount</td>
-          <td>Date</td>
-          <td>Notes</td>
-          <td>Blank</td>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Groceries</td>
-            <td>$50</td>
-            <td>11/03/2022</td>
-            <td>Weekly groceries</td>
-            <td>Edit/Delete</td>
-          </tr>
-          <tr>
-            <td>Eating out</td>
-            <td>$1,000,000</td>
-            <td>03/03/2022</td>
-            <td>Dinner with Frankie</td>
-            <td>Edit/Delete</td>
-          </tr>
-        </tbody>
-      </table>
+      <div class="dashboard-headings">
+        <label for="">Expense</label>
+        <label for="">Amount</label>
+        <label for="">Date</label>
+        <label for="">Notes</label>
+      </div>
+      <form class="">
+        <input type="text" id="#" name="category" for="Expense" size="10" />
+        <input type="number" id="#" name="amount" for="Amount" size="10" />
+        <input type="date" id="#" name="datePaid" for="Date" size="10" />
+        <input type="text" id="#" name="notes" for="Notes" size="10" />
+      </form>
     </div>
   );
 }
